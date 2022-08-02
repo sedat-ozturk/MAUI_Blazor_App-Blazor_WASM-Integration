85c1aa62311b294ea5400065a2c2e489e4b98132
Sedat ÖZTÜRK <gamebooker@gmail.com>
2022-08-02 22:53:27 +0300

    Blazorise.DataGrid projeye dahil edildi.

--- ListForm.BlazorWasm/Program.cs
+++ ListForm.BlazorWasm/Program.cs
@@ -3,6 +3,9 @@ using ListForm.BlazorWasm.Services;
 using ListForm.Shared.Interfaces;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,4 +13,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IPlatformService, PlatformService>();
+builder.Services.AddBlazorise(options => { options.Immediate = true; }).AddBootstrapProviders().AddFontAwesomeIcons();
+
 await builder.Build().RunAsync();
