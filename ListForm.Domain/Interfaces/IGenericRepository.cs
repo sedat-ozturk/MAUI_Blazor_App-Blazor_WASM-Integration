9ffcd633851e7010633cfebd86610dd2c1353050
Sedat ÖZTÜRK <gamebooker@gmail.com>
2022-08-03 11:36:41 +0300

    GetAllAsync Default Value verildi.

--- ListForm.Domain/Interfaces/IGenericRepository.cs
+++ ListForm.Domain/Interfaces/IGenericRepository.cs
@@ -6,7 +6,7 @@ namespace ListForm.Domain.Interfaces
     public interface IGenericRepository<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
-        Task<List<T>> GetAllAsync(int skip, int take, string order);
+        Task<List<T>> GetAllAsync(int skip = 0, int take = 10, string order = "");
         Task<int> AddAsync(T entity);
         Task<int> UpdateAsync(T entity);
         Task<int> DeleteAsync(int id);
