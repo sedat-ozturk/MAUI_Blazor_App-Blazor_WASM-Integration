47e5995fac01c7f2a64885c185eb3b2c03979664
Sedat ÖZTÜRK <gamebooker@gmail.com>
2022-08-03 12:24:41 +0300

    IProductRepository ve ProductRepository aip üzerinden inject edildi.

--- ListForm.Api/Program.cs
+++ ListForm.Api/Program.cs
@@ -14,6 +14,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors();
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
