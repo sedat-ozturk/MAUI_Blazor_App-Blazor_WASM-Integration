604712a48122dacf7fbe90686856c3d39aab4f62
Sedat Öztürk <sedat.ozturk@sozsoft.com>
2022-08-04 23:58:41 +0300

    ConnectionString değiştirildi.

---
+++ ListForm.Domain/DefaultConnectionString.cs
@@ -0,0 +1,10 @@
+﻿namespace ListForm.Domain
+{
+    public static class AppSettings
+    {
+        public static string DefaultConnectionString()
+        {
+            return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ListForm;Trusted_Connection=True;MultipleActiveResultSets=True";
+        }
+    }
+}
